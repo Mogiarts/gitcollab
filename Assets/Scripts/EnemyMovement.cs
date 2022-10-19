9fee4d799395d7bfa66077abf3b3251d9ae7efc9
DisturbedBanana <90250325+DisturbedBanana@users.noreply.github.com>
2022-10-19 09:02:12 +0200

    Fixed EnemyMovement, does not rotate the Enemy for now.
    Successfully translates Enemy towards player.

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -6,21 +6,11 @@ using UnityEngine.UIElements;
 public class EnemyMovement : MonoBehaviour
 {
 
-    Transform target;
+    public Transform target;
     public float speed = 3f;
-    public float rotationSpeed = 3f;
 
     private void FixedUpdate()
     {
-        target = GameObject.FindWithTag("Player").transform;
-        Vector3 targetHeading = target.position - transform.position;
-        Vector3 targetDirection = targetHeading.normalized;
-
-        //rotate to look at the player
-
-        transform.rotation = Transform.LookAt(target.transform.position, new Vector3(0,0,0));
-
-        //move towards the player
-        transform.position += transform.forward * speed * Time.deltaTime;
+        transform.Translate((target.position - transform.position).normalized * speed * Time.deltaTime);
     }
 }
