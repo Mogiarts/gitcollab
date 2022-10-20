7bcd670c8c8a18e2696af0f39b60a9deff5f983a
RakanAD <zhangroland385@gmail.com>
2022-10-20 09:51:26 +0200

    Added Tag Bullet + Enemy
    Update Bullet.cs, Kill Enemy

--- Assets/Scripts/Bullets.cs
+++ Assets/Scripts/Bullets.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class Bullets : MonoBehaviour
 {
     private Rigidbody2D rb;
+    //private BoxCollider2D bc;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        //bc = gameObject.AddComponent<BoxCollider2D>();
+    }
 
+    private void OnTriggerEnter2D(Collider2D enemy)
+    {
+        if (enemy.CompareTag("Enemy"))
+        {
+            Destroy(enemy.gameObject);
+        }
     }
     public void Shoot(Vector3 direction)
     {
