5b2936912fcb21b7a6feda8d0e54d0ed07787c0a
DisturbedBanana <90250325+DisturbedBanana@users.noreply.github.com>
2022-10-21 11:12:18 +0200

    Bullets are now destroyed if more than 4 seconds old

--- Assets/Scripts/Bullets.cs
+++ Assets/Scripts/Bullets.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class Bullets : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private float _timeAlive = 0f;
     //private BoxCollider2D bc;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         //bc = gameObject.AddComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        _timeAlive += Time.deltaTime;
+        if (_timeAlive > 4f)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D enemy)
